aa2388839a08d3a99adddf1e6c814f1ecda4a32e
gaticks <kenneth.norholm@gmail.com>
2019-08-13 10:01:52 +0200

    Created hello world project for JSON..NET json serializer for issue CC-18

---
+++ hello-world-json-net-serializer/hello-world-json-net-serializer/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hello_world_json_net_serializer
+{
+    public class Account
+    {
+        public string Email { get; set; }
+        public bool Active { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public IList<string> Roles { get; set; }
+    }
+}
---
+++ hello-world-json-net-serializer/hello-world-json-net-serializer/Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace hello_world_json_net_serializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account
+            {
+                Email = "james@example.com",
+                Active = true,
+                CreatedDate = new DateTime(2013, 1, 20, 0, 0, 0, DateTimeKind.Utc),
+                Roles = new List<string> {"User","Admin" }
+            };
+
+            string json = JsonConvert.SerializeObject(account, Formatting.Indented );
+            Console.WriteLine(json);
+            Console.ReadKey();  
+        }
+    }
+}
