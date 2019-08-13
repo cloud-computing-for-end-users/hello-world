d12fb7dc5157f12d240ee39c769410c6380954f5
gaticks <kenneth.norholm@gmail.com>
2019-08-13 09:04:42 +0200

    the fully created NetMq hello world project according to CC-15

---
+++ NetMq-helloWorld/NetMQ-hello-world-server/Program.cs
@@ -0,0 +1,53 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System;
+using System.Threading;
+
+namespace NetMQ_hello_world_server
+{
+    class Program
+    {
+        const int ListeningPort = 55555;
+
+        /// <summary>
+        /// protocol for NetMq
+        /// the <message> should be a string send as the second frame and the message must not contain more that two frames or it will be discarded by the server
+        /// "A" as first frame if you want <message> + " hello" as response
+        /// "B" as first frame if you want "hello " + <message>
+        /// </summary>
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("NetMQ server have started");
+
+
+            var server = new ResponseSocket();
+
+            server.Bind("tcp://*:" + ListeningPort);
+            while (true)
+            {
+                var message = server.ReceiveMultipartMessage();
+                if(message.FrameCount == 2)
+                {
+                    var frameOne = message[0];
+                    Console.WriteLine("recived message with protocol code: " + frameOne.ConvertToString());
+                    
+                    //sending response
+                    if (frameOne.ConvertToString() == "A") {
+                        server.SendFrame(message[1].ConvertToString() + " hello");
+                        continue;
+                    }
+                    else if(frameOne.ConvertToString() == "B"){
+                        server.SendFrame(message[1].ConvertToString() + " hello");
+                        continue;
+                    }
+
+                   
+                }
+
+                //must only occure if the message is not valid according to the protocol
+                    server.SendFrame("INVALID FRAME COUNT!");
+            }
+        }
+    }
+}
---
+++ NetMq-helloWorld/NetMq-helloWorld/Program.cs
@@ -0,0 +1,53 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System;
+
+namespace NetMq_helloWorld
+{
+    class Program
+    {
+
+        /// <summary>
+        /// protocol for NetMq
+        /// the <message> should be a string send as the second frame and the message must not contain more that two frames or it will be discarded by the server
+        /// "A" as first frame if you want <message> + "hello" as response
+        /// "B" as first frame if you want "hello" + <message>
+        /// </summary>
+
+        const int portToConnectTo = 55555;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("NetMq hello wolrd client have started:");
+
+            using(var client = new RequestSocket())
+            {
+                client.Connect("tcp://localhost:" + portToConnectTo);
+                
+                Console.WriteLine("Sending message, using 'A' as the first frame");
+
+
+                client.SendMoreFrame("A").SendFrame("I am saying");
+
+                var message = client.ReceiveFrameString();
+                Console.WriteLine("Received {0}", message);
+
+                Console.WriteLine("Sending message, using 'B' as the first frame");
+
+                client.SendMoreFrame("B").SendFrame(", world!");
+
+                message = client.ReceiveFrameString();
+                Console.WriteLine("Received {0}", message);
+
+                client.SendMoreFrame("C").SendFrame(", world!");
+
+                message = client.ReceiveFrameString();
+                Console.WriteLine("Received {0}", message);
+
+
+                //waiting so the tester can see the console 
+                Console.ReadKey();
+            }
+        }
+    }
+}
