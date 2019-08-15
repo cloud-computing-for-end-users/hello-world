81c483b2b975b265ebd9e58f669ff74105458f47
gaticks <kenneth.norholm@gmail.com>
2019-08-15 08:49:36 +0200

    Created a hello world program for transfering files using the NetMQ Client-Server structure for issue CC-21

---
+++ transfer-file-NetMQ-hello-world/transfer-file-NetMQ-hello-world-server/Program.cs
@@ -0,0 +1,45 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System;
+using System.IO;
+
+namespace transfer_file_NetMQ_hello_world_server
+{
+    class Program
+    {
+        private const string path = @"C:\Users\MSI\Desktop\";
+
+        static void Main(string[] args)
+        {
+            using(var server = new ResponseSocket())
+            {
+                server.Bind("tcp://*:5555");
+                Console.WriteLine("The application have initialised");
+
+                var message = server.ReceiveMultipartMessage();
+                var messagePartOne = message[0].ConvertToString();
+
+                if ("FILE".Equals( messagePartOne ))
+                {
+                    var fileName = message[1].ConvertToString();
+                    var data = message[2].ToByteArray();
+
+
+                    Console.WriteLine("Frame 0:" + message[0].ConvertToString());
+                    Console.WriteLine("Frame 1:" + fileName);
+                    Console.WriteLine("Frame 2:" + data.Length);
+
+
+                    var stream = File.OpenWrite(path + fileName);
+                    stream.Write(data, 0, data.Length);
+                    stream.Flush();
+                    stream.Close();
+
+
+                    Console.WriteLine("finished writing the file");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
---
+++ transfer-file-NetMQ-hello-world/transfer-file-NetMQ-hello-world/Program.cs
@@ -0,0 +1,41 @@
+﻿using NetMQ.Sockets;
+using NetMQ;
+using System;
+using System.IO;
+
+namespace transfer_file_NetMQ_hello_world
+{
+    class Program
+    {
+        private const string path = @"C:\Users\MSI\Desktop\bla\testFileToSend.txt";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Client initilizing");
+
+            using (var client = new RequestSocket())
+            {
+                client.Connect("tcp://localhost:5555");
+
+                byte[] data = File.ReadAllBytes(path);
+
+                var f1 = "FILE";
+                var f2 = "testFileToSend.txt";
+                var f3 = data;
+
+
+                client.SendMoreFrame(f1).SendMoreFrame(f2).SendFrame(f3);
+
+
+                Console.WriteLine("Frame 1:" + f1);
+                Console.WriteLine("Frame 2:" + f2);
+                Console.WriteLine("Frame 3 - length:" + f3.Length);
+
+                Console.WriteLine("finished sending file");
+
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
