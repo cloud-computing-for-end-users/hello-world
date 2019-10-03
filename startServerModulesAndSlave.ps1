0d7ae2c073f666066a319bf1689dc8d6cb96ccd2
Krystof Spiller <253812@via.dk>
2019-10-03 11:15:43 +0200

    CC-10 Fixed path

--- startServerModulesAndSlave.ps1
+++ startServerModulesAndSlave.ps1
@@ -1,18 +1,17 @@
 
 $RunServerModules = $True # $False
-cd $PSScriptRoot
 
 if($RunServerModules) {
     # server module
-    cd ..\server-module\src\server-module\server-module
+    cd $PSScriptRoot\..\server-module\src\server-module\server-module
     Start-Process cmd.exe -ArgumentList "/C dotnet run && pause"
     Start-Sleep -Seconds 7
 
     # slave owner servermodule
-    cd ..\slave-owner-servermodule\src\slave-owner-servermodule\slave-owner-servermodule
+    cd $PSScriptRoot\..\slave-owner-servermodule\src\slave-owner-servermodule\slave-owner-servermodule
     Start-Process cmd.exe -ArgumentList "/C dotnet run && pause"
     Start-Sleep -Seconds 7
 }
 # slave module
-cd ..\\slave-module\src\slave-controller
+cd $PSScriptRoot\..\slave-module\src\slave-controller
 Start-Process cmd.exe -ArgumentList "/C dotnet run && pause"
